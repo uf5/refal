d35c45d78209cd5abbcb64f7a5c0ff360ba5c3c6
uf5 <notbailey13@gmail.com>
2024-03-17 11:50:11 +0300

    Refactor pattern matching

--- src/Language/Refal/PatternMatching.hs
+++ src/Language/Refal/PatternMatching.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Language.Refal.PatternMatching (matchPattern, Substitution) where
 
 import Control.Applicative
@@ -11,47 +9,36 @@ matchPattern :: [PatternExpression] -> [ObjectExpression] -> Maybe [Substitution
 matchPattern = matchPattern' []
 
 matchPattern' :: [Substitution] -> [PatternExpression] -> [ObjectExpression] -> Maybe [Substitution]
-matchPattern' subs [] = \case
-  [] -> pure subs
-  _ -> Nothing
-matchPattern' subs ((PSym p) : ps) = \case
-  ((OSym o) : os) ->
-    if p == o
+matchPattern' subs [] [] = pure subs
+matchPattern' subs ((PSym p) : ps) ((OSym o) : os) =
+  if p == o
+    then matchPattern' subs ps os
+    else Nothing
+matchPattern' subs ((PVar v@(Var S _)) : ps) (ox@(OSym _) : os) = case lookup v subs of
+  Just defined ->
+    if ox == defined
       then matchPattern' subs ps os
       else Nothing
-  _ -> Nothing
-matchPattern' subs ((PVar v@(Var S _)) : ps) = \case
-  (ox@(OSym _) : os) -> case lookup v subs of
-    Just defined ->
-      if ox == defined
-        then matchPattern' subs ps os
-        else Nothing
-    Nothing -> matchPattern' ((v, ox) : subs) ps os
-  _ -> Nothing
-matchPattern' subs ((PVar v@(Var T _)) : ps) = \case
-  (ox : os) -> case lookup v subs of
-    Just defined ->
-      if ox == defined
-        then matchPattern' subs ps os
-        else Nothing
-    Nothing -> matchPattern' ((v, ox) : subs) ps os
-  _ -> Nothing
-matchPattern' subs ((PVar v@(Var E _)) : ps) = \case
-  o@(ox : os) -> case lookup v subs of
-    (Just defined) ->
-      if ox == defined
-        then matchPattern' subs ps os
-        else Nothing
-    _ -> takeUntilNextMatches [] o
-  _ -> Nothing
+  Nothing -> matchPattern' ((v, ox) : subs) ps os
+matchPattern' subs ((PVar v@(Var T _)) : ps) (ox : os) = case lookup v subs of
+  Just defined ->
+    if ox == defined
+      then matchPattern' subs ps os
+      else Nothing
+  Nothing -> matchPattern' ((v, ox) : subs) ps os
+matchPattern' subs ((PVar v@(Var E _)) : ps) o@(ox : os) = case lookup v subs of
+  (Just defined) ->
+    if ox == defined
+      then matchPattern' subs ps os
+      else Nothing
+  _ -> takeUntilNextMatches [] o
   where
-    takeUntilNextMatches elts o =
-      matchPattern' ((v, OSt elts) : subs) ps o
-        <|> case o of
-          (ox : os) -> takeUntilNextMatches (elts <> [ox]) os
+    takeUntilNextMatches elts o' =
+      matchPattern' ((v, OSt elts) : subs) ps o'
+        <|> case o' of
+          (ox' : os') -> takeUntilNextMatches (elts <> [ox']) os'
           [] -> pure [(v, OSt elts)]
-matchPattern' subs ((PSt p) : ps) = \case
-  ((OSt o) : os) -> do
-    subs' <- matchPattern' subs p o
-    matchPattern' subs' ps os
-  _ -> Nothing
+matchPattern' subs ((PSt p) : ps) ((OSt o) : os) = do
+  subs' <- matchPattern' subs p o
+  matchPattern' subs' ps os
+matchPattern' _ _ _ = Nothing
