29721fafc492e347704d474ce497aab64b7c1450
uf5 <notbailey13@gmail.com>
2024-06-13 20:51:27 +0300

    Replace adding new substitutions using monoid with pushType

--- src/Language/Refal/PatternMatching/Implementations/Recursive.hs
+++ src/Language/Refal/PatternMatching/Implementations/Recursive.hs
@@ -17,13 +17,13 @@ matchPattern' subs@Substitutions {sType = ss} ((PVar (SType v)) : ps) ((OSym sx)
     Just defined
       | sx == defined -> matchPattern' subs ps os
       | otherwise -> Nothing
-    Nothing -> matchPattern' (mempty {sType = [(v, sx)]} <> subs) ps os
+    Nothing -> matchPattern' (pushSType v sx subs) ps os
 matchPattern' subs@Substitutions {tType = ts} ((PVar (TType v)) : ps) (ox : os) =
   case lookup v ts of
     Just defined
       | ox == defined -> matchPattern' subs ps os
       | otherwise -> Nothing
-    Nothing -> matchPattern' (mempty {tType = [(v, ox)]} <> subs) ps os
+    Nothing -> matchPattern' (pushTType v ox subs) ps os
 matchPattern' subs@Substitutions {eType = es} ((PVar (EType v)) : ps) o =
   case lookup v es of
     Just defined
@@ -33,7 +33,7 @@ matchPattern' subs@Substitutions {eType = es} ((PVar (EType v)) : ps) o =
       | otherwise -> Nothing
     Nothing -> choice $ map (withEVar . (`splitAt` o)) [0 .. length o]
   where
-    withEVar (elts, rest) = matchPattern' (mempty {eType = [(v, elts)]} <> subs) ps rest
+    withEVar (elts, rest) = matchPattern' (pushEType v elts subs) ps rest
 matchPattern' subs ((PSt p) : ps) ((OSt o) : os) = do
   subs' <- matchPattern' subs p o
   matchPattern' subs' ps os
